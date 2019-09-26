bc1091ee58f292a2f929b3a8bbfda3e82901c6fc
Yves Reynhout <yves.reynhout@gmail.com>
2019-09-26 16:16:43 +0200

    feat(eventmapping): adds try get event name and try get event type

--- src/Be.Vlaanderen.Basisregisters.EventHandling/EventMapping.cs
+++ src/Be.Vlaanderen.Basisregisters.EventHandling/EventMapping.cs
@@ -12,6 +12,8 @@ namespace Be.Vlaanderen.Basisregisters.EventHandling
 
         public EventMapping(IReadOnlyDictionary<string, Type> eventTypeMapping)
         {
+            if (eventTypeMapping == null) throw new ArgumentNullException(nameof(eventTypeMapping));
+
             _eventNameTypeMapping = eventTypeMapping.ToDictionary(x => x.Key, x => x.Value);
             _typeEventNameMapping = eventTypeMapping.ToDictionary(x => x.Value, x => x.Key);
         }
@@ -29,24 +31,24 @@ namespace Be.Vlaanderen.Basisregisters.EventHandling
 
         public Type GetEventType(string eventName)
         {
-            if (_eventNameTypeMapping == null)
-                throw new InvalidOperationException("De event mapping is nog niet geconfigureerd.");
-
             if (_eventNameTypeMapping.ContainsKey(eventName))
                 return _eventNameTypeMapping[eventName];
 
             throw new KeyNotFoundException($"Type voor event met naam '{eventName}' niet gevonden.");
         }
 
+        public bool TryGetEventType(string eventName, out Type eventType) =>
+            _eventNameTypeMapping.TryGetValue(eventName, out eventType);
+
         public string GetEventName(Type eventType)
         {
-            if (_typeEventNameMapping == null)
-                throw new InvalidOperationException("De event mapping is nog niet geconfigureerd.");
-
             if (_typeEventNameMapping.ContainsKey(eventType))
                 return _typeEventNameMapping[eventType];
 
             throw new KeyNotFoundException($"Naam voor event met type '{eventType}' niet gevonden.");
         }
+
+        public bool TryGetEventName(Type eventType, out string eventName) =>
+            _typeEventNameMapping.TryGetValue( eventType, out eventName);
     }
 }
