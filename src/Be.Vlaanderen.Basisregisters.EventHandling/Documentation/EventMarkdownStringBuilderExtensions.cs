1887e5298b546e66b08239c46df0679a96d3a9a1
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-12-01 18:34:08 +0100

    fix: remove empty lines when no event description is set GRAR-1613

--- src/Be.Vlaanderen.Basisregisters.EventHandling/Documentation/EventMarkdownStringBuilderExtensions.cs
+++ src/Be.Vlaanderen.Basisregisters.EventHandling/Documentation/EventMarkdownStringBuilderExtensions.cs
@@ -1,6 +1,7 @@
 namespace Be.Vlaanderen.Basisregisters.EventHandling.Documentation
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
 
     internal static class EventMarkdownStringBuilderExtensions
@@ -12,24 +13,34 @@ namespace Be.Vlaanderen.Basisregisters.EventHandling.Documentation
 
         public static StringBuilder AppendInfo(this StringBuilder builder, IEnumerable<EventInformation> eventInfos)
         {
-            foreach (var info in eventInfos)
+            foreach (var info in eventInfos.Where(i => i != null))
                 builder
-                    .AppendLine($"## {info.Name}")
-                    .AppendLine()
-                    .AppendLine(info.Description)
-                    .AppendLine()
-                    .AppendInfo(info.Properties);
+                    .AppendEventName(info)
+                    .AppendEventDescription(info)
+                    .AppendProperties(info);
 
             return builder;
         }
 
-        private static StringBuilder AppendInfo(this StringBuilder builder, IEnumerable<EventPropertyInformation> eventPropertyInfos)
+        private static StringBuilder AppendEventName(this StringBuilder builder, EventInformation info)
+            => builder
+                .AppendLine($"## {info.Name}")
+                .AppendLine();
+
+        private static StringBuilder AppendEventDescription(this StringBuilder builder, EventInformation info)
+            => string.IsNullOrWhiteSpace(info.Description)
+                ? builder
+                : builder
+                    .AppendLine(info.Description)
+                    .AppendLine();
+
+        private static StringBuilder AppendProperties(this StringBuilder builder, EventInformation info)
         {
             builder
                 .AppendLine("| Attribuut | Omschrijving |")
                 .AppendLine("|---|---|");
 
-            foreach (var property in eventPropertyInfos)
+            foreach (var property in info.Properties.Where(p => p != null))
                 builder.AppendLine($"| {property.Name} | {property.Description} |");
 
             return builder.AppendLine();
