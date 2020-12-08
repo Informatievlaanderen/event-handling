35f29b3833a8a3ec99c335c2141344c0540149b5
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-12-08 17:20:57 +0100

    feat: add generating markdown for specific tags
    BREAKING CHANGES
    added function to the public Interface

--- src/Be.Vlaanderen.Basisregisters.EventHandling/Documentation/IRegistryEventsMarkdownGenerator.cs
+++ src/Be.Vlaanderen.Basisregisters.EventHandling/Documentation/IRegistryEventsMarkdownGenerator.cs
@@ -1,10 +1,14 @@
 namespace Be.Vlaanderen.Basisregisters.EventHandling.Documentation
 {
+    using System.Collections.Generic;
     using System.Text;
 
     public interface IRegistryEventsMarkdownGenerator
     {
         string Generate();
+		
+        string GenerateFor(IEnumerable<EventTag> tags);
+		
         internal IRegistryEventsMarkdownGenerator CreateDuplicateUsing(StringBuilder externalMarkdownBuilder);
     }
 }
