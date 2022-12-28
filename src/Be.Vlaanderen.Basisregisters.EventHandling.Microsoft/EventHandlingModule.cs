8bfc9c9c64ca0a40c66aee51922d3291c8864364
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-12-28 15:56:34 +0100

    fix: add Microsoft.Extensions.DependencyInjection

---
+++ src/Be.Vlaanderen.Basisregisters.EventHandling.Microsoft/EventHandlingModule.cs
@@ -0,0 +1,31 @@
+using Be.Vlaanderen.Basisregisters.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Be.Vlaanderen.Basisregisters.EventHandling.Microsoft
+{
+    using System.Reflection;
+    using Newtonsoft.Json;
+
+    public class EventHandlingModule : IServiceCollectionModule
+    {
+        private readonly EventDeserializer _eventDeserializer;
+        private readonly EventSerializer _eventSerializer;
+        private readonly EventMapping _eventMapping;
+
+        public EventHandlingModule(Assembly eventsAssembly, JsonSerializerSettings jsonSerializerSettingsForEvents)
+        {
+            _eventDeserializer = new EventDeserializer((eventData, eventType) => JsonConvert.DeserializeObject(eventData, eventType, jsonSerializerSettingsForEvents));
+            _eventSerializer = new EventSerializer(@event => JsonConvert.SerializeObject(@event, jsonSerializerSettingsForEvents));
+            _eventMapping = new EventMapping(EventMapping.DiscoverEventNamesInAssembly(eventsAssembly));
+        }
+
+        public void Load(IServiceCollection services)
+        {
+            services.AddTransient(_ => _eventMapping);
+
+            services.AddTransient(_ => _eventDeserializer);
+
+            services.AddTransient(_ => _eventSerializer);
+        }
+    }
+}
---
+++ src/Be.Vlaanderen.Basisregisters.EventHandling.Microsoft/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Vlaamse basisregisters")]
+[assembly: AssemblyDescription("Autofac module to help with setting up a lightweight event handling infrastructure.")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("b50bd9da-b3dd-46e4-a71a-4ffd9969750d")]
+
+[assembly: NeutralResourcesLanguage("en")]
