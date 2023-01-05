84539c91e36b689a6abbe26c794e06a8515e0f28
Arne Dumarey <dumarey.arne@gmail.com>
2023-01-05 19:45:54 +0100

    feat: add direct microsoft ioc registration

--- src/Be.Vlaanderen.Basisregisters.EventHandling.Microsoft/EventHandlingModule.cs
+++ src/Be.Vlaanderen.Basisregisters.EventHandling.Microsoft/EventHandlingModule.cs
@@ -7,24 +7,18 @@ namespace Be.Vlaanderen.Basisregisters.EventHandling.Microsoft
 
     public class EventHandlingModule : IServiceCollectionModule
     {
-        private readonly EventDeserializer _eventDeserializer;
-        private readonly EventSerializer _eventSerializer;
-        private readonly EventMapping _eventMapping;
+        private readonly Assembly _eventsAssembly;
+        private readonly JsonSerializerSettings _jsonSerializerSettingsForEvents;
 
         public EventHandlingModule(Assembly eventsAssembly, JsonSerializerSettings jsonSerializerSettingsForEvents)
         {
-            _eventDeserializer = new EventDeserializer((eventData, eventType) => JsonConvert.DeserializeObject(eventData, eventType, jsonSerializerSettingsForEvents));
-            _eventSerializer = new EventSerializer(@event => JsonConvert.SerializeObject(@event, jsonSerializerSettingsForEvents));
-            _eventMapping = new EventMapping(EventMapping.DiscoverEventNamesInAssembly(eventsAssembly));
+            _eventsAssembly = eventsAssembly;
+            _jsonSerializerSettingsForEvents = jsonSerializerSettingsForEvents;
         }
 
         public void Load(IServiceCollection services)
         {
-            services.AddTransient(_ => _eventMapping);
-
-            services.AddTransient(_ => _eventDeserializer);
-
-            services.AddTransient(_ => _eventSerializer);
+            services.ConfigureEventHandling(_eventsAssembly, _jsonSerializerSettingsForEvents);
         }
     }
 }
