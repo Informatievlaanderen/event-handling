d406d5a91574bbcaca5f13421be21360291efd53
jvandaal <jonas.vandaal@outlook.be>
2024-03-08 09:34:01 +0100

    feat: move to dotnet 8.0.2
    move to dotnet 8.0.2
    
    BREAKING CHANGE: move to dotnet 8.0.2

--- src/Be.Vlaanderen.Basisregisters.EventHandling.Autofac/Properties/AssemblyInfo.cs
+++ src/Be.Vlaanderen.Basisregisters.EventHandling.Autofac/Properties/AssemblyInfo.cs
@@ -5,8 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyProduct("Vlaamse basisregisters")]
 [assembly: AssemblyDescription("Autofac module to help with setting up a lightweight event handling infrastructure.")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
 [assembly: AssemblyCompany("Vlaamse overheid")]
 
